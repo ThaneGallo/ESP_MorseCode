c8b776b8ac1314563d44efacb2303174367cad50
ThaneGallo <thanegallo@gmail.com>
2024-07-26 15:06:43 -0400

    logs properly removed / commented pieces that dont work

--- touch_button/main/touch_button_example_main.c
+++ touch_button/main/touch_button_example_main.c
@@ -7,64 +7,69 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-
 #include "esp_log.h"
 
 static int messageBuffer[10];
 static uint8_t buf_end = 0;
 
+static uint64_t start_time;
+static uint64_t end_time;
+
+
 static QueueHandle_t gpio_evt_queue = NULL;
 
-#define GPIO_INPUT_IO_0 CONFIG_GPIO_INPUT_0
-#define GPIO_INPUT_IO_1 4
+#define GPIO_INPUT_IO_0 4
+#define GPIO_INPUT_IO_1 5
+#define GPIO_INPUT_IO_2 23
+
 #define GPIO_INPUT_PIN_SEL ((1ULL << GPIO_INPUT_IO_0) | (1ULL << GPIO_INPUT_IO_1))
 #define ESP_INTR_FLAG_DEFAULT 0
+#define MORSE_TAG "Morse code tag"
 
-static void gpio_send_buffer(void *arg)
+static void IRAM_ATTR gpio_start_event_handler(void *arg)
 {
-    int i; 
-    printf("in send buffer isr");
-
-    for(i = 0; i <= buf_end; i++){
-        printf("%i", messageBuffer[i]);
-    }
-
-    buf_end = 0;
-    printf("\n");
+   
 }
 
-static void gpio_fill_buffer(void *arg)
+static void IRAM_ATTR gpio_end_event_handler(void *arg)
 {
-    int check1 = 0;
-
-    printf("in fill buffer isr");
-
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    check1 = gpio_get_level(GPIO_INPUT_IO_0);
-
-    //bc button pulls low
-    if (!check1)
-    {
-        messageBuffer[buf_end] = 1;
-        buf_end++;
-    }
-    else
-    {
-        messageBuffer[buf_end] = 0;
-        buf_end++;
-    }
+   
 }
 
-static void gpio_task_example(void* arg)
+
+
+static void IRAM_ATTR gpio_fill_buffer(void *arg)
 {
-     uint32_t io_num;
+    // int check1 = 0;
+
+    // check1 = gpio_get_level(GPIO_INPUT_IO_0);
+
+    // // bc button pulls low
+    // if (!check1)
+    // {
+    //     messageBuffer[buf_end] = 1;
+    //     buf_end++;
+    // }
+    // else
+    // {
+    //     messageBuffer[buf_end] = 0;
+    //     buf_end++;
+    // }
+
+    ESP_DRAM_LOGI(MORSE_TAG, "inside fill buffer");
+}
+
+static void gpio_task_example(void *arg)
+{
+    uint32_t io_num;
 
     printf("in gpio task");
 
-    for (;;) { // ;; = infinite loop
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) // if the queue is recieved a task  
-         {
-            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+    for (;;)
+    {                                                              // ;; = infinite loop
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) // if the queue is recieved a task
+        {
+            printf("GPIO[%" PRIu32 "] intr, val: %d\n", io_num, gpio_get_level(io_num));
         }
     }
 }
@@ -84,14 +89,14 @@ void app_main(void)
     io_conf.pull_up_en = 1;
     gpio_config(&io_conf);
 
-    //makes event queue
-    //create a queue to handle gpio event from isr
+    // makes event queue
+    // create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
 
     // install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
 
     // hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_fill_buffer, (void *)GPIO_INPUT_IO_0);
@@ -100,15 +105,12 @@ void app_main(void)
     // only need one to send buffer
     gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_send_buffer, (void *)GPIO_INPUT_IO_1);
 
-    // start buffer reading text with element read until long wait period (5 sec) of no readings
-    // second button to send message (1 to type the other to encode/decode)
-    // spit out final bufer / decoded text
- 
+   
+
     int cnt = 0;
-    while (1) {
+    while (1)
+    {
         printf("cnt: %d\n", cnt++);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-
-
 }
