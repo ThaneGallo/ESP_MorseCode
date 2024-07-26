f09e6c0a4e2a77a986dac81cb651cbdd74e68e0b
ThaneGallo <thanegallo@gmail.com>
2024-07-26 15:30:06 -0400

    fixed button debounce

--- touch_button/main/touch_button_example_main.c
+++ touch_button/main/touch_button_example_main.c
@@ -6,15 +6,16 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_timer.h"
+
 #include "driver/gpio.h"
 #include "esp_log.h"
 
 static int messageBuffer[10];
 static uint8_t buf_end = 0;
 
-static uint64_t start_time;
-static uint64_t end_time;
-
+static int64_t start_time;
+static int64_t end_time;
 
 static QueueHandle_t gpio_evt_queue = NULL;
 
@@ -28,35 +29,28 @@ static QueueHandle_t gpio_evt_queue = NULL;
 
 static void IRAM_ATTR gpio_start_event_handler(void *arg)
 {
-   
+    static int64_t lMillis = 0;
+
+    if ((esp_timer_get_time() - lMillis) < 5)
+        return;
+
+    lMillis = esp_timer_get_time();
+
+    start_time = esp_timer_get_time();
+    ESP_DRAM_LOGI(MORSE_TAG, "start time: %d", start_time);
 }
 
 static void IRAM_ATTR gpio_end_event_handler(void *arg)
 {
-   
-}
+    static int64_t lMillis = 0;
 
+    if ((esp_timer_get_time() - lMillis) < 5)
+        return;
 
+    lMillis = esp_timer_get_time();
 
-static void IRAM_ATTR gpio_fill_buffer(void *arg)
-{
-    // int check1 = 0;
-
-    // check1 = gpio_get_level(GPIO_INPUT_IO_0);
-
-    // // bc button pulls low
-    // if (!check1)
-    // {
-    //     messageBuffer[buf_end] = 1;
-    //     buf_end++;
-    // }
-    // else
-    // {
-    //     messageBuffer[buf_end] = 0;
-    //     buf_end++;
-    // }
-
-    ESP_DRAM_LOGI(MORSE_TAG, "inside fill buffer");
+    end_time = esp_timer_get_time();
+    ESP_DRAM_LOGI(MORSE_TAG, "end time: %d", end_time);
 }
 
 static void gpio_task_example(void *arg)
@@ -80,9 +74,19 @@ void app_main(void)
     gpio_config_t io_conf = {};
 
     // interrupt of falling edge
-    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
     // bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    io_conf.pin_bit_mask = (1ULL << GPIO_INPUT_IO_0);
+    // set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    // enable high on default
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    // interrupt of falling edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    // bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = (1ULL << GPIO_INPUT_IO_1);
     // set as input mode
     io_conf.mode = GPIO_MODE_INPUT;
     // enable high on default
@@ -99,13 +103,11 @@ void app_main(void)
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
 
     // hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_fill_buffer, (void *)GPIO_INPUT_IO_0);
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_start_event_handler, (void *)GPIO_INPUT_IO_0);
 
     // hook isr handler for specific gpio pin
     // only need one to send buffer
-    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_send_buffer, (void *)GPIO_INPUT_IO_1);
-
-   
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_end_event_handler, (void *)GPIO_INPUT_IO_1);
 
     int cnt = 0;
     while (1)
