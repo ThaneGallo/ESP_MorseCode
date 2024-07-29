a69a24777fc484381f20a1dd1485a6265a5b8d77
ThaneGallo <thanegallo@gmail.com>
2024-07-29 17:06:14 -0400

    added return statements to return character

--- touch_button/main/touch_button.c
+++ touch_button/main/touch_button.c
@@ -126,119 +126,119 @@ static void IRAM_ATTR gpio_send_event_handler(void *arg)
     buf_end = 0;
 }
 
-void processMorseCode(int decimalValue) {
+char processMorseCode(int decimalValue) {
     switch (decimalValue) {
         case 5:
             // Handle case for A: .-
-            break;
+            return 'a';
         case 24:
             // Handle case for B: -..
-            break;
+            return 'b';
         case 26:
             // Handle case for C: -.-.
-            break;
+            return 'c';
         case 12:
             // Handle case for D: -..
-            break;
+            return 'd';
         case 2:
             // Handle case for E: .
-            break;
+            return 'e';
         case 18:
             // Handle case for F: ..-.
-            break;
+            return 'f';
         case 14:
             // Handle case for G: --.
-            break;
+            return 'g';
         case 16:
             // Handle case for H: ....
-            break;
+            return 'h';
         case 4:
             // Handle case for I: ..
-            break;
+            return 'i';
         case 23:
             // Handle case for J: .---
-            break;
+            return 'j';
         case 13:
             // Handle case for K: -.- 
-            break;
+            return 'k';
         case 20:
             // Handle case for L: .-..
-            break;
+            return 'l';
         case 7:
             // Handle case for M: --
-            break;
+            return 'm';
         case 6:
             // Handle case for N: -.
-            break;
+            return 'n';
         case 15:
             // Handle case for O: ---
-            break;
+            return 'o';
         case 22:
             // Handle case for P: .--.
-            break;
+            return 'p';
         case 29:
             // Handle case for Q: --.-
-            break;
+            return 'q';
         case 10:
             // Handle case for R: .-.
-            break;
+            return 'r';
         case 8:
             // Handle case for S: ...
-            break;
+            return 's';
         case 3:
             // Handle case for T: -
-            break;
+            return 't';
         case 9:
             // Handle case for U: ..-
-            break;
+            return 'u';
         case 17:
             // Handle case for V: ...-
-            break;
+            return 'v';
         case 11:
             // Handle case for W: .--
-            break;
+            return 'w';
         case 25:
             // Handle case for X: -..-
-            break;
+            return 'x';
         case 27:
             // Handle case for Y: -.-- 
-            break;
+            return 'y';
         case 28:
             // Handle case for Z: --..
-            break;
+            return 'z';
         case 63:
             // Handle case for 0: -----
-            break;
+            return '0';
         case 47:
             // Handle case for 1: .----
-            break;
+            return '1';
         case 39:
             // Handle case for 2: ..---
-            break;
+            return '2';
         case 35:
             // Handle case for 3: ...--
-            break;
+            return '3';
         case 33:
             // Handle case for 4: ....-
-            break;
+            return '4';
         case 64:
             // Handle case for 5: .....
-            break;
+            return '5';
         case 48:
             // Handle case for 6: -....
-            break;
+            return '6';
         case 56:
             // Handle case for 7: --...
-            break;
+            return '7';
         case 60:
             // Handle case for 8: ---..
-            break;
+            return '8';
         case 61:
             // Handle case for 9: ----.
-            break;
+            return '9';
         default:
             // Handle unknown cases
-            break;
+            return '=';
     }
 }
 
@@ -249,8 +249,8 @@ static void gpio_task_example(void *arg)
 
     printf("in gpio task");
 
-    for (;;)
-    {                                                              // ;; = infinite loop
+    for (;)
+    {                                                              // ; = infinite loop
         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) // if the queue is recieved a task
         {
             printf("GPIO[%" PRIu32 "] intr, val: %d\n", io_num, gpio_get_level(io_num));
